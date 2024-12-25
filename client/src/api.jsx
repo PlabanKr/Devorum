b50f2f34f1b555b7d692174e9e4eccd5e59d9c04
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-12-25 15:33:02 +0530

    BAD FIXgit add . NOT SECURE AT ALL

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const url = import.meta.env.VITE_SERVER_URL;
 
 const api = axios.create({
-  baseURL: url + '/api/v1',
+  baseURL: 'https://devorum-backend-7ngp.onrender.com/api/v1',
 });
 
 export default api;
