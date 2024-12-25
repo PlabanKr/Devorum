0f76fa1d84cb5d9437c7bd11d24088bb7ef9db4a
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-12-25 15:16:07 +0530

    Use variable to store the url

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const url = import.meta.env.VITE_SERVER_URL;
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_URL + '/api/v1',
+  baseURL: url + '/api/v1',
 });
 
 export default api;
